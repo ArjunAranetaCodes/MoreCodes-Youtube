30469cfb5489e1eb30ae882f08698d657ee2aaca
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-01-25 05:00:00 +0100

    2018 Updates

---
+++ react-flask-mongodb-login-reg/client/src/components/UserFunctions.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+export const register = newUser => {
+    return axios
+        .post("users/register", {
+            first_name: newUser.first_name,
+            last_name: newUser.last_name,
+            email: newUser.email,
+            password: newUser.password
+        })
+        .then(response => {
+            console.log("Registered")
+        })
+}
+
+export const login = user => {
+    return axios
+        .post("users/login", {
+            email: user.email,
+            password: user.password
+        })
+        .then(response => {
+            localStorage.setItem('usertoken', response.data.token)
+            return response.data.token
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
