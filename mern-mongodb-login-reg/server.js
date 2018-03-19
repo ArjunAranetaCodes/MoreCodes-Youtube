b992c4c21dfd1274915f456e8f79a1f550d9c71c
Arjun Araneta <arjunaraneta@gmail.com>
2018-03-19 05:00:00 +0100

    2018 Updates

---
+++ mern-mongodb-login-reg/server.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+var app = express()
+const mongoose = require('mongoose')
+var port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+
+const mongoURI = 'mongodb://localhost:27017/mernloginreg'
+
+mongoose
+  .connect(
+    mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+var Users = require('./routes/Users')
+
+app.use('/users', Users)
+
+app.listen(port, function() {
+  console.log('Server is running on port: ' + port)
+})
