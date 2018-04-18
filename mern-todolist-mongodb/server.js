e501093fd1418a0c0b74b07444e1118ac07c6ae4
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-18 05:00:00 +0100

    2018 Updates

---
+++ mern-todolist-mongodb/server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var index = require("./routes/index");
+var tasks = require("./routes/tasks");
+
+const cors = require("cors");
+
+var port = 5000;
+
+var app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  })
+);
+
+//View Engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "client")));
+
+// Body Parser MW
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", index);
+app.use("/api", tasks);
+
+app.listen(port, function() {
+  console.log("Server started on port " + port);
+});
