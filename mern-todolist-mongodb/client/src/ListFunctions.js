c7a1a7d4789eae970597c180dcba51732b6a54c0
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-20 05:00:00 +0100

    2018 Updates

---
+++ mern-todolist-mongodb/client/src/ListFunctions.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+export const getList = () => {
+  return axios
+    .get("http://localhost:5000/api/tasks", {
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(res => {
+      return res.data;
+    });
+  /*.then(data => {
+      console.log(data);
+    });*/
+};
+
+export const addToList = term => {
+  return axios
+    .post(
+      "http://localhost:5000/api/task",
+      {
+        title: term,
+        isDone: false
+      },
+      {
+        headers: { "Content-Type": "application/json" }
+      }
+    )
+    .then(function(response) {
+      console.log(response);
+    });
+};
+
+export const deleteItem = term => {
+  axios
+    .delete(`http://localhost:5000/api/task/${term}`, {
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
+
+export const updateItem = (term, id) => {
+  return axios
+    .put(
+      `http://localhost:5000/api/task/${id}`,
+      {
+        title: term,
+        isDone: false
+      },
+      {
+        headers: { "Content-Type": "application/json" }
+      }
+    )
+    .then(function(response) {
+      console.log(response);
+    });
+};
