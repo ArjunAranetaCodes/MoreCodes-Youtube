5de1229b59c9be0685d0035763243ba75441ae9a
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-17 05:00:00 +0100

    2018 Updates

---
+++ mern-todolist-mongodb-redux/client/src/index.1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+//import App from './App'
+import ReduxTest from './ReduxTest'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<ReduxTest />, document.getElementById('root'))
+registerServiceWorker()
