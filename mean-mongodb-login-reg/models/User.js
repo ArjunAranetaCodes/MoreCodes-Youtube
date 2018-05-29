439a37785ba61f939e26d162f93ee51f42094a08
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-29 05:00:00 +0100

    2018 Updates

---
+++ mean-mongodb-login-reg/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Create Schema
+const UserSchema = new Schema({
+  first_name: {
+    type: String
+  },
+  last_name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = User = mongoose.model('users', UserSchema)
