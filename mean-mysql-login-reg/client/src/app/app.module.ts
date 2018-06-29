54228130c251ab9337ddeb1b2f88c05411e0fbcc
Arjun Araneta <arjunaraneta@gmail.com>
2018-06-29 05:00:00 +0100

    2018 Updates

---
+++ mean-mysql-login-reg/client/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router'
+
+import { AppComponent } from './app.component'
+import { ProfileComponent } from './profile/profile.component'
+import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component'
+import { HomeComponent } from './home/home.component'
+import { AuthenticationService } from './authentication.service'
+import { AuthGuardService } from './auth-guard.service'
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuardService]
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [AuthenticationService, AuthGuardService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
