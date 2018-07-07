70081dbdcd88c58ec172c009254df4148ef8dedd
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-07-07 05:00:00 +0100

    2018 Updates

---
+++ vuejs-flask-mongodb-login-reg/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <div class="jumbotron mt-5">
+            <div class="col-sm-8 mx-auto">
+                <h1 class="text-center">WELCOME</h1>
+            </div>
+        </div>
+    </div>
+</template>
