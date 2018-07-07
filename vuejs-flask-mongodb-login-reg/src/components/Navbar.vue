e62c52b36101973c0a1a4a5ec17f0c430a749a24
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-07-07 05:00:00 +0100

    2018 Updates

---
+++ vuejs-flask-mongodb-login-reg/src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbar1"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbar1">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/">Home</router-link>
+                </li>
+
+                <li v-if="auth==''" class="nav-item">
+                    <router-link class="nav-link" to="/login">Login</router-link>
+                </li>
+
+                <li v-if="auth==''" class="nav-item">
+                    <router-link class="nav-link" to="/register">Register</router-link>
+                </li>
+
+                <li v-if="auth=='loggedin'" class="nav-item">
+                    <router-link class="nav-link" to="/profile">Profile</router-link>
+                </li>
+
+                <li v-if="auth=='loggedin'" class="nav-item">
+                    <a href="" class="nav-link" v-on:click="logout">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+import EventBus from './EventBus'
+
+EventBus.$on('logged-in', test => {
+  console.log(test)
+})
+
+export default {
+  data () {
+    return {
+      auth: '',
+      user: ''
+    }
+  },
+
+  methods: {
+    logout () {
+      localStorage.removeItem('usertoken')
+    }
+  },
+  mounted () {
+    EventBus.$on('logged-in', status => {
+      this.auth = status
+    })
+  }
+}
+
+</script>
