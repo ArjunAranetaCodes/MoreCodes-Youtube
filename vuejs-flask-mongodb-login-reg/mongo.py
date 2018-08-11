2aac905fdcfd41731f0d5fc5360dc1ad5552b8e9
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-08-11 05:00:00 +0100

    2018 Updates

---
+++ vuejs-flask-mongodb-login-reg/mongo.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, request, jsonify
+from flask_pymongo import PyMongo 
+from bson.objectid import ObjectId 
+from datetime import datetime 
+from flask_cors import CORS 
+from flask_bcrypt import Bcrypt 
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'vueloginreg'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/vueloginreg'
+app.config['JWT_SECRET_KEY'] = 'secret'
+
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+CORS(app)
+
+@app.route('/users/register', methods=['POST'])
+def register():
+    users = mongo.db.users 
+    first_name = request.get_json()['first_name']
+    last_name = request.get_json()['last_name']
+    email = request.get_json()['email']
+    password = bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
+    created = datetime.utcnow()
+
+    user_id = users.insert({
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'password': password,
+        'created': created 
+    })
+
+    new_user = users.find_one({'_id': user_id})
+
+    result = {'email': new_user['email'] + ' registered'}
+    
+    return jsonify({'result' : result})
+
+@app.route('/users/login', methods=['POST'])
+def login():
+    users = mongo.db.users 
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    result = ""
+
+    response = users.find_one({'email': email})
+
+    if response:
+        if bcrypt.check_password_hash(response['password'], password):
+            access_token = create_access_token(identity = {
+                'first_name': response['first_name'],
+                'last_name': response['last_name'],
+                'email': response['email']
+            })
+            result = jsonify({"token": access_token})
+        else:
+            result = jsonify({"error": "Invalid username and password"})
+    else:
+        result = jsonify({"result": "No results found"})
+    return result 
+
+if __name__ == '__main__':
+    app.run(debug=True)
