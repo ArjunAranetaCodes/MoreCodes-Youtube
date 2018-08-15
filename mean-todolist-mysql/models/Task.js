eb92d182fcd7af35a814263ac84d1b7938416ad1
Arjun Araneta <arjunaraneta@gmail.com>
2018-08-15 05:00:00 +0100

    2018 Updates

---
+++ mean-todolist-mysql/models/Task.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const db = require("../database/db.js");
+
+module.exports = db.sequelize.define(
+  "tbl_tasks",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    task_name: {
+      type: Sequelize.STRING
+    }
+  },
+  {
+    timestamps: false
+  }
+);
