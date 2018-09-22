78c6899e45320cbbbac63e6838c2588c35d8b690
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-22 05:00:00 +0100

    2018 Updates

---
+++ vuejs-flask-mysql-login-reg/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    'Navbar': Navbar
+  }
+}
+</script>
