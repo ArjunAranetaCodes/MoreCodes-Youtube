c1c68130b394735b3711dc5448dd891ae99802f6
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-09-30 05:00:00 +0100

    2018 Updates

---
+++ adonisjs-react-mongodb-todolist/client/src/components/Profile.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import jwt_decode from 'jwt-decode'
+import { getUser } from './UserFunctions'
+
+class Profile extends Component {
+  constructor() {
+    super()
+    this.state = {
+      first_name: '',
+      last_name: '',
+      email: '',
+      errors: {}
+    }
+  }
+
+  componentDidMount() {
+    const token = localStorage.usertoken
+    const decoded = jwt_decode(token)
+    getUser(decoded.uid).then(res => {
+      this.setState({
+        first_name: res.data.first_name,
+        last_name: res.data.last_name,
+        email: res.data.email
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron mt-5">
+          <div className="col-sm-8 mx-auto">
+            <h1 className="text-center">PROFILE</h1>
+          </div>
+          <table className="table col-md-6 mx-auto">
+            <tbody>
+              <tr>
+                <td>Fist Name</td>
+                <td>{this.state.first_name}</td>
+              </tr>
+              <tr>
+                <td>Last Name</td>
+                <td>{this.state.last_name}</td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>{this.state.email}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Profile
