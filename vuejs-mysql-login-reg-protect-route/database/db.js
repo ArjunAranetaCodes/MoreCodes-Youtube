f23d3afe256bb013a2e3034339b63d8959a433a5
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-12 05:00:00 +0100

    2018 Updates

---
+++ vuejs-mysql-login-reg-protect-route/database/db.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize')
+const db = {}
+const sequelize = new Sequelize('nodejs_login1', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
