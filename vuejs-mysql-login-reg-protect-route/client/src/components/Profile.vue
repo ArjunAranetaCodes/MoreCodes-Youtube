09e392044aa48b80ccb5488b6d2ffd8cc69657de
Arjun Araneta <arjunaraneta@gmail.com>
2018-11-05 05:00:00 +0100

    2018 Updates

---
+++ vuejs-mysql-login-reg-protect-route/client/src/components/Profile.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="jumbotron mt-5">
+      <div class="col-sm-8 mx-auto">
+        <h1 class="text-center">PROFILE</h1>
+      </div>
+      <table class="table col-md-6 mx-auto">
+        <tbody>
+          <tr>
+            <td>Fist Name</td>
+            <td>{{first_name}}</td>
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td>{{last_name}}</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>{{email}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import router from '../router'
+import axios from 'axios'
+
+export default {
+  data () {
+    const token = localStorage.usertoken
+
+    return {
+      first_name: '',
+      last_name: '',
+      email: '',
+      token: token
+    }
+  },
+  methods: {
+    getuser () {
+      axios.get('/users/profile', {
+        headers: { 'Authorization': this.token }
+      }).then(res => {
+        this.first_name = res.data.first_name
+        this.last_name = res.data.last_name
+        this.email = res.data.email
+      }).catch(err => {
+        console.log(err)
+        router.push({ name: 'Login' })
+      })
+    }
+  },
+  mounted () {
+    this.getuser()
+  }
+}
+</script>
