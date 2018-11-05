4c220b2d93bf71d4fe10d422c8add97fe8c7f26c
Arjun Araneta <arjunaraneta@gmail.com>
2018-11-05 05:00:00 +0100

    2018 Updates

---
+++ vuejs-mysql-login-reg-protect-route/client/src/components/Register.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 mt-5 mx-auto">
+        <form v-on:submit.prevent="register">
+          <h1 class="h3 mb-3 font-weight-normal">Register</h1>
+          <div class="form-group">
+            <label for="name">First name</label>
+            <input type="text" v-model="first_name" class="form-control" name="first_name" placeholder="Enter your first name">
+          </div>
+          <div class="form-group">
+            <label for="name">Last name</label>
+            <input type="text" v-model="last_name" class="form-control" name="last_name" placeholder="Enter your lastname name">
+          </div>
+          <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" v-model="email" class="form-control" name="email" placeholder="Enter email">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" v-model="password" class="form-control" name="password" placeholder="Password">
+          </div>
+          <button type="submit" class="btn btn-lg btn-primary btn-block">Register!</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    register () {
+      axios.post('/users/register',
+        {
+          first_name: this.first_name,
+          last_name: this.last_name,
+          email: this.email,
+          password: this.password
+        }
+      ).then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  }
+
+}
+</script>
