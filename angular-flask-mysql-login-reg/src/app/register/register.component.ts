cf420cd06a026ad7790715cfc4fe86ca136543b9
Arjun Araneta <arjunaraneta@gmail.com>
2018-11-16 05:00:00 +0100

    2018 Updates

---
+++ angular-flask-mysql-login-reg/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+import { AuthenticationService, TokenPayload } from "../authentication.service";
+import { Router } from "@angular/router";
+
+@Component({
+  templateUrl: "./register.component.html"
+})
+export class RegisterComponent {
+  credentials: TokenPayload = {
+    id: 0,
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: ""
+  };
+
+  constructor(private auth: AuthenticationService, private router: Router) {}
+
+  register() {
+    this.auth.register(this.credentials).subscribe(
+      () => {
+        this.router.navigateByUrl("/profile");
+      },
+      err => {
+        console.error(err);
+      }
+    );
+  }
+}
