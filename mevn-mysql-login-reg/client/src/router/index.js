01a916a842cf3d9d0578f83aa22cb24088c9c6a3
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-02-03 05:00:00 +0800

    2019 Updates

---
+++ mevn-mysql-login-reg/client/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import Profile from '@/components/Profile'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    }
+  ]
+})
---
+++ mevn-mysql-login-reg/routes/Users.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const users = express.Router()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/User')
+users.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+
+users.post('/register', (req, res) => {
+  const today = new Date()
+  const userData = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,
+    created: today
+  }
+
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(user => {
+      if (!user) {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          userData.password = hash
+          User.create(userData)
+            .then(user => {
+              res.json({ status: user.email + 'Registered!' })
+            })
+            .catch(err => {
+              res.send('error: ' + err)
+            })
+        })
+      } else {
+        res.json({ error: 'User already exists' })
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
+})
+
+users.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(user => {
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+            expiresIn: 1440
+          })
+          res.send(token)
+        }
+      } else {
+        res.status(400).json({ error: 'User does not exist' })
+      }
+    })
+    .catch(err => {
+      res.status(400).json({ error: err })
+    })
+})
+
+module.exports = users
