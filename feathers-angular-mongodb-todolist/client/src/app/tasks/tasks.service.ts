92f1c69acd3a513e70c6c28018d5da87d92207d3
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-04-04 05:00:00 +0800

    2019 Updates

---
+++ feathers-angular-mongodb-todolist/client/src/app/tasks/tasks.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+import { Observable } from 'rxjs'
+
+import { Task } from './task'
+
+@Injectable()
+export class TasksService {
+  constructor(private http: HttpClient) { }
+
+  getTasks (): Observable<Task[]> {
+    return this.http.get<Task[]>('api/tasks')
+  }
+
+  addTask (task: Task): Observable<Task> {
+    console.log(task)
+    return this.http.post<Task>('api/task', task)
+  }
+
+  deleteTask (id: number): Observable<{}> {
+    const url = `api/task/${id}`
+
+    return this.http.delete(url)
+  }
+
+  updateTask (task: Task): Observable<Task> {
+    const url = `api/task/${task._id}`
+    return this.http.put<Task>(url, task)
+  }
+}
