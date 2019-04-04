9ddc100ae9d038306fc7b5424822c9ce9c43c98f
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-04-05 05:00:00 +0800

    2019 Updates

---
+++ feathers-angular-mongodb-todolist/client/src/app/tasks/tasks.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core'
+
+import { Task } from './task'
+import { TasksService } from './tasks.service'
+
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  providers: [TasksService]
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[]
+  editTask: Task
+
+  constructor(private taskService: TasksService, private http: HttpClient) { }
+
+  ngOnInit () {
+    this.getTasks()
+  }
+
+  getTasks (): void {
+    this.taskService.getTasks().subscribe(tasks => (this.tasks = tasks))
+  }
+
+  add (task_name: string): void {
+    this.editTask = undefined
+    task_name = task_name.trim()
+    if (!task_name) {
+      return
+    }
+
+    const newTask: Task = { task_name } as Task
+    this.taskService.addTask(newTask).subscribe(() => this.getTasks())
+  }
+
+  delete (task: Task): void {
+    this.tasks = this.tasks.filter(h => h !== task)
+    this.taskService
+      .deleteTask(task._id)
+      .subscribe(() => console.log('Task deleted'))
+  }
+
+  edit (task) {
+    this.editTask = task
+  }
+
+  update () {
+    if (this.editTask) {
+      this.taskService.updateTask(this.editTask).subscribe(() => {
+        this.getTasks()
+      })
+      this.editTask = undefined
+    }
+  }
+}
