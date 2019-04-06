97b44485999821d6a480441078d518377ba50422
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-04-07 05:00:00 +0800

    2019 Updates

---
+++ feathers-react-mongodb-todolist/client/src/ListFunctions.js
@@ -0,0 +1,62 @@
+import axios from 'axios'
+
+export const getList = () => {
+  return axios
+    .get('api/tasks', {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => {
+      var data = []
+      Object.keys(res.data).forEach(function(key) {
+        var val = res.data[key]
+        data.push([val.task_name, val._id])
+      })
+
+      return data
+    })
+}
+
+export const addToList = term => {
+  return axios
+    .post(
+      'api/task',
+      {
+        task_name: term
+      },
+      {
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
+    .then(function(response) {
+      console.log(response)
+    })
+}
+
+export const deleteItem = term => {
+  axios
+    .delete(`api/task/${term}`, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(function(response) {
+      console.log(response)
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
+}
+
+export const updateItem = (term, id) => {
+  return axios
+    .put(
+      `api/task/${id}`,
+      {
+        task_name: term
+      },
+      {
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
+    .then(function(response) {
+      console.log(response)
+    })
+}
