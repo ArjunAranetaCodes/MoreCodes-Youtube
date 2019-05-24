ba6d036599fe16bfeeeb63fc379252bf708f778d
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-05-25 05:00:00 +0800

    2019 Updates

---
+++ feathers-vuejs-mongodb-login-reg/client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <navbar></navbar>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    'Navbar': Navbar
+  }
+}
+</script>
