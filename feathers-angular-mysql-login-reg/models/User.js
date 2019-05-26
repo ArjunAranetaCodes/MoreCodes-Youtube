e6e959f9edceaa47b467319b53bf25d2463fc991
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-05-27 05:00:00 +0800

    2019 Updates

---
+++ feathers-angular-mysql-login-reg/models/User.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('../database/db.js')
+
+module.exports = db.sequelize.define(
+  'user',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    first_name: {
+      type: Sequelize.STRING
+    },
+    last_name: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    created: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  },
+  {
+    timestamps: false
+  }
+)
