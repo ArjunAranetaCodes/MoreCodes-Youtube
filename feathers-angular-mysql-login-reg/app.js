69d3f3b612f1564114457e581619ff3f0d3a95f5
ArjunAranetaCodes <arjunaraneta@gmail.com>
2019-05-27 05:00:00 +0800

    2019 Updates

---
+++ feathers-angular-mysql-login-reg/app.js
@@ -0,0 +1,16 @@
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const user = require('./routes/Users')
+
+const app = express(feathers());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.configure(express.rest());
+
+app.use(express.errorHandler());
+app.use('/users', user);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started at ${port}`);
+});
